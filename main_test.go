825371f91baaf13518a93a7a77c341dca1b8fcdb
bas <tima.bashirov92@gmail.com>
2025-10-27 11:12:08 +0300

    feat(main_test): add TestCafeCount and TestCafeSearch for cafe endpoint validation

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -46,5 +47,68 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	var requests []struct {
+		count int
+		want  int
+	}
+	var actual int
+
+	for city, cafes := range cafeList {
+		requests = []struct {
+			count int
+			want  int
+		}{
+			{0, 0},
+			{1, 1},
+			{2, 2},
+			{100, len(cafes)},
+		}
+		for _, v := range requests {
+			response := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/cafe?city="+city+"&count="+strconv.Itoa(v.count), nil)
+
+			handler.ServeHTTP(response, req)
+
+			if response.Body.String() != "" {
+				reqCafes := strings.Split(response.Body.String(), ",")
+				actual = len(reqCafes)
+			} else {
+				actual = 0
+			}
+			assert.Equal(t, v.want, actual, "for city=%q, count=%d", city, v.count)
+		}
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	var requests = []struct {
+		search    string
+		wantCount int
+	}{
+		{"фасоль", 0},
+		{"кофе", 2},
+		{"вилка", 1},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/cafe?city=moscow&search="+v.search, nil)
+
+		handler.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+
+		var got []string
+		if response.Body.String() != "" {
+			got = strings.Split(response.Body.String(), ",")
+		}
+		assert.Equal(t, v.wantCount, len(got), "for search=%q", v.search)
 	}
 }
